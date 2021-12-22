5f2e0c9f215e827fa66c5a5677c6d4150d7bb089
hzy <1257664828@qq.com>
2021-12-22 11:12:46 +0800

    ✨ feat(同步代码): 同步gitee和github代码

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:codehzy/learn-typescript.git master:gh-pages
+git push -f git@gitee.com:codehzy/learn-typescript.git master:gh-pages
 
 cd -
