cf07a08ba5de04697e2255a61774610f57d17e44
hzy <1257664828@qq.com>
2021-12-22 10:33:15 +0800

    ✨ feat(搭建项目): 测试vuepress搭建博客

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  title: 'TypeScript4 文档',
+  description: 'TypeScript4 最新官方文档翻译',
+  theme: 'reco',
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+  base: '/learn-typescript/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+
+      {
+        text: 'JavaScript博客',
+        items: [
+          { text: 'Github', link: 'https://github.com/codehzy' },
+          { text: '掘金', link: 'https://juejin.cn/user/1714893872178823' }
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        title: '欢迎学习',
+        path: '/',
+        collapsable: false, //折叠
+        children: [{ title: '学前必读', path: '/' }]
+      },
+      {
+        title: '基础学习',
+        path: '/handbook/ConditionalTypes',
+        collapsable: false, //折叠
+        children: [
+          { title: '条件类型', path: '/handbook/ConditionalTypes' },
+          { title: '泛型', path: '/handbook/Generics' }
+        ]
+      }
+    ],
+    // 开始多级标题展示
+    subSideBar: 'auto'
+  }
+}
